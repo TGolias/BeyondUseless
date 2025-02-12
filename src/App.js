e196c2afd33405524541ab996a14390e41fbaff1
Tyler Golias <tgolias94@gmail.com>
2025-02-11 23:26:19 -0500

    fix blur and get rid of ugly scrolling logic

--- src/App.js
+++ src/App.js
@@ -106,9 +106,6 @@ export default function App() {
   const [history, setHistory] = useState([startingPlayerConfigs]);
   const [currentHistoryIndex, setCurrentHistoryIndex] = useState(0);
 
-  const [disableScrolling, setDisableScrolling] = useState(false);
-  const [scrollTop, setScrollTop] = useState(undefined);
-
   let hideEditorLocalStorage = localStorage.getItem("HIDE_EDITOR");
   const startingValueForHideEditor = hideEditorLocalStorage === "true";
 
@@ -410,18 +407,6 @@ export default function App() {
     }
   ]
 
-  if ((showStartMenu || centerScreenMenu.show)) {
-    turnScrollingOff(disableScrolling, setDisableScrolling, scrollTop);
-    if (window.onscroll != blockedWindowScroll) {
-      window.onscroll = blockedWindowScroll;
-    }
-  } else {
-    turnScrollingOn(disableScrolling, setDisableScrolling, scrollTop);
-    if (window.onscroll == blockedWindowScroll || window.onscroll == undefined) {
-      window.onscroll = () => normalWindowScroll(setScrollTop);
-    }
-  }
-
   return (
     <>
       <div className={"topDiv" + ((showStartMenu || centerScreenMenu.show) ? " disableActivity" : "")} onScroll={() => { 
@@ -458,49 +443,4 @@ export default function App() {
       </div>
     </>
   );
-}
-
-function turnScrollingOff(disableScrolling, setDisableScrolling, scrollTop) {
-  if (!disableScrolling) {
-    disableScrolling = true;
-    setDisableScrolling(true);
-
-    var doc = document.documentElement;
-    doc.style.width = 'calc(100% - '+ getScrollbarSize() +'px)';
-    doc.style.position = 'fixed';
-    doc.style.top = -scrollTop + 'px';
-    doc.style.overflow = 'hidden';
-  }
-}
-
-function turnScrollingOn(disableScrolling, setDisableScrolling, scrollTop) {
-  if (disableScrolling) {
-    disableScrolling = false;
-    setDisableScrolling(false);
-    
-    var doc = document.documentElement;
-    doc.style.width = '';
-    doc.style.position = '';
-    doc.style.top = '';
-    doc.style.overflow = '';
-    
-    window.scroll(0, scrollTop);
-  }
-}
-
-function getScrollbarSize() {
-  var doc = document.documentElement;
-  var dummyScroller = document.createElement('div');
-  dummyScroller.setAttribute('style', 'width:99px;height:99px;' + 'position:absolute;top:-9999px;overflow:scroll;');
-  doc.appendChild(dummyScroller);
-  const scrollBarSize = dummyScroller.offsetWidth - dummyScroller.clientWidth;
-  doc.removeChild(dummyScroller);
-  return scrollBarSize;
-}
-
-function blockedWindowScroll() {
-}
-
-function normalWindowScroll(setScrollTop) {
-  setScrollTop(document.documentElement.scrollTop);
 }
