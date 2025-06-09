c7944b9465d30732ce6a9e57d30f8d0afccf72bb
Tyler Golias <tgolias94@gmail.com>
2025-06-09 15:17:21 -0400

    null check

--- src/Components/DesignerComponents/FeatDesign.js
+++ src/Components/DesignerComponents/FeatDesign.js
@@ -8,21 +8,23 @@ export function FeatDesign({baseStateObject, inputHandler, selectedFeatName, fea
     const featContent = [];
 
     const selectedFeat = feats.find(x => x.name === selectedFeatName);
-    if (selectedFeat.choices) {
-        featContent.push(<>
-            <ChoiceDesign baseStateObject={baseStateObject} choiceObject={selectedFeat} pathToPlayerConfigObjectForChoices={pathToFeatureProperty} inputHandler={inputHandler}></ChoiceDesign>
-        </>);
-    }
-
-    if (selectedFeat?.aspects?.features && selectedFeat.aspects.features.length > 0) {
-        for (let i = 0; i < selectedFeat.aspects.features.length; i++) {
-            const featFeature = selectedFeat.aspects.features[i];
-            const featFeatureName = featFeature.name.replace(/\s/g, "");
-            const pathToFeatFeature = pathToFeatureProperty + ".features." + featFeatureName;
-            const playerFeatFeatureObject = getValueFromObjectAndPath(baseStateObject, pathToFeatFeature);
+    if (selectedFeat) {
+        if (selectedFeat.choices) {
             featContent.push(<>
-                <FeatureDesign baseStateObject={baseStateObject} feature={featFeature} playerFeatureObject={playerFeatFeatureObject} pathToFeatureProperty={pathToFeatFeature} inputHandler={inputHandler} parameters={parameters}></FeatureDesign>
+                <ChoiceDesign baseStateObject={baseStateObject} choiceObject={selectedFeat} pathToPlayerConfigObjectForChoices={pathToFeatureProperty} inputHandler={inputHandler}></ChoiceDesign>
             </>);
+        }
+
+        if (selectedFeat?.aspects?.features && selectedFeat.aspects.features.length > 0) {
+            for (let i = 0; i < selectedFeat.aspects.features.length; i++) {
+                const featFeature = selectedFeat.aspects.features[i];
+                const featFeatureName = featFeature.name.replace(/\s/g, "");
+                const pathToFeatFeature = pathToFeatureProperty + ".features." + featFeatureName;
+                const playerFeatFeatureObject = getValueFromObjectAndPath(baseStateObject, pathToFeatFeature);
+                featContent.push(<>
+                    <FeatureDesign baseStateObject={baseStateObject} feature={featFeature} playerFeatureObject={playerFeatFeatureObject} pathToFeatureProperty={pathToFeatFeature} inputHandler={inputHandler} parameters={parameters}></FeatureDesign>
+                </>);
+            }
         }
     }
 
